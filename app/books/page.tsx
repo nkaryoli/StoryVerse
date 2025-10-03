7b09d67bd724054ee188ef6033ef8fc7bc142f13
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-10-04 00:21:03 +0200

    add: shuffled function to randomise all book list

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -8,16 +8,18 @@ const BooksPage = async () => {
   const allBooksResults = await Promise.all(allBooksPromises);
 
   const allBooks = allBooksResults.flat();
+  const shuffledBooks = [...allBooks].sort(() => Math.random() - 0.5);
 
   return (
     <section className="w-fit space-y-9 m-auto">
       <h1 className="text-3xl font-bold text-white capitalize">Todos los Libros</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">         {allBooks.map((book: Book, index: number) => (
-        <BookCard
-          book={book}
-          key={index}
-        />
-      ))}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
+        {shuffledBooks.map((book: Book, index: number) => (
+          <BookCard
+            book={book}
+            key={index}
+          />
+        ))}
       </div>
     </section>
   );
